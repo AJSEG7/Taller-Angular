2d0f0afe967c8ac1e9f676a6c756c14a9187b44f
Alejandro Segura <a.segurat@uniandes.edu.co>
2023-05-04 15:31:36 -0500

    Taller finalizado

---
+++ src/app/series/series.service.ts
@@ -0,0 +1,21 @@
+import {HttpClient} from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Serie } from './Serie';
+import { environment } from "src/enviroments/enviroments";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeriesService {
+
+  private apiURL = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getSeries(): Observable<Serie[]>{
+    return this.http.get<Serie[]>(this.apiURL);
+  }
+
+}
